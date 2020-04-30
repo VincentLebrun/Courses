78631c29005fa3b8baf58d2208452e395d9fd361
VincentLebrun <lebrunvincent12@gmail.com>
2020-04-30 15:51:55 +0200

    This is my first exercices i created with what i've learned alone, a game type role playing game. It's just the first interface to ask some information about the player. note : " i created this without commentary next time i'll add them"

---
+++ exercices_1.py
@@ -0,0 +1,23 @@
+age = int(input("Quel est votre age?"))
+username = input('Quel est le nom de votre héros?')
+
+while 1:
+ sexe = input("Choisissez le sexe de votre personnage en tapant M pour Masculin et F pour féminin.")
+ if sexe in ( "F" , "M", "f","m"):
+  break
+ else:
+  print("Faites un choix bon sang!")
+
+user_choice = int(input("Choisissez votre classe parmi les suivantes ""1=Humain 2=Elfe 3=Orque 4=Troll"))
+if   user_choice == 1:
+     race = "Humain"
+elif user_choice == 2:
+     race = "Elfe"
+elif user_choice == 3:
+     race = "Orque"
+elif user_choice == 4:
+     race = "Troll"
+else:
+    print("Il n'y a que 4 choix c'est pourtant pas compliqué!")
+
+print(username,"Vous avez rejoint la race des ", race, "dans un petit village nommé Rakda")
