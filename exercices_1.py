49c015fc348c3dcd059f8e5187c0ff892a4baddd
VincentLebrun <lebrunvincent12@gmail.com>
2020-05-01 12:52:00 +0200

    New indentation with four spaces for 1 indentation

--- exercices_1.py
+++ exercices_1.py
@@ -2,22 +2,23 @@ age = int(input("Quel est votre age?"))
 username = input('Quel est le nom de votre héros?')
 
 while 1:
- sexe = input("Choisissez le sexe de votre personnage en tapant M pour Masculin et F pour féminin.")
- if sexe in ( "F" , "M", "f","m"):
-  break
- else:
-  print("Faites un choix bon sang!")
+    sexe = input("Choisissez le sexe de votre personnage en tapant M pour Masculin et F pour féminin.")
+    if sexe in ( "F" , "M", "f","m"):
+        break
+    else:
+        print("Faites un choix bon sang!")
 
 user_choice = int(input("Choisissez votre classe parmi les suivantes ""1=Humain 2=Elfe 3=Orque 4=Troll"))
 if   user_choice == 1:
-     race = "Humain"
+    race = "Humain"
 elif user_choice == 2:
-     race = "Elfe"
+    race = "Elfe"
 elif user_choice == 3:
-     race = "Orque"
+    race = "Orque"
 elif user_choice == 4:
-     race = "Troll"
+    race = "Troll"
 else:
     print("Il n'y a que 4 choix c'est pourtant pas compliqué!")
 
 print(username,"Vous avez rejoint la race des ", race, "dans un petit village nommé Rakda")
+
